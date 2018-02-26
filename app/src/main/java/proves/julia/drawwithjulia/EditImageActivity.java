7ccd5d3ebb3183869b6b3cb713d3789a30c05196
julia <julia@reset.cat>
2018-02-26 17:55:36 +0100

    Added draw attributes

--- app/src/main/java/proves/julia/drawwithjulia/EditImageActivity.java
+++ app/src/main/java/proves/julia/drawwithjulia/EditImageActivity.java
@@ -4,8 +4,11 @@ import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,27 +24,27 @@ import com.byox.drawview.enums.DrawingTool;
 import com.byox.drawview.views.DrawView;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created by julia on 2/14/18.
  */
 
-public class EditImageActivity extends Activity {
+public class EditImageActivity extends AppCompatActivity {
 
     private DrawView myDrawView;
     private FrameLayout parentLayout;
     private LinearLayout pencilButton, undoButton, redoButton, eraseButton, textButton,
             figuresButton, rightLayout, lineButton, arrowButton, rectangleButton,
-            circleButton, ellipseButton;
-    private ImageView figuresImage;
+            circleButton, ellipseButton, drawAttrButton;
+    private ImageView figuresImage, backgroundImage;
     private TextView figuresText;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_image);
-
-        Bitmap bitmap;
 
         setLayouts();
 
@@ -52,9 +55,28 @@ public class EditImageActivity extends Activity {
             myDrawView.post(new Runnable() {
                 @Override
                 public void run() {
+                    try {
 
-                    if (filepath != null && !filepath.isEmpty())
-                        myDrawView.setBackgroundImage(new File(filepath), BackgroundType.FILE, BackgroundScale.CENTER_CROP);
+                        BitmapFactory.Options options = new BitmapFactory.Options();
+                        options.inJustDecodeBounds = true;
+
+                        //Set phone metrics
+                        DisplayMetrics displaymetrics = new DisplayMetrics();
+                        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+
+                        options.inSampleSize = 2;
+                        options.inJustDecodeBounds = false;
+
+                        bitmap = BitmapFactory.decodeFile(filepath, options);
+
+                        backgroundImage.setImageBitmap(bitmap);
+                        myDrawView.setBackgroundColor(Color.TRANSPARENT);
+
+                    } catch (NullPointerException e) {
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             });
 
@@ -73,6 +95,7 @@ public class EditImageActivity extends Activity {
 
         parentLayout = (FrameLayout) findViewById(R.id.parentLayout);
         rightLayout = (LinearLayout) findViewById(R.id.rightLayout);
+        drawAttrButton = (LinearLayout) findViewById(R.id.drawAttrButton);
         pencilButton = (LinearLayout) findViewById(R.id.pencilButton);
         undoButton = (LinearLayout) findViewById(R.id.undoButton);
         redoButton = (LinearLayout) findViewById(R.id.redoButton);
@@ -88,7 +111,16 @@ public class EditImageActivity extends Activity {
         figuresImage = (ImageView) findViewById(R.id.figuresImage);
         figuresText = (TextView) findViewById(R.id.figuresText);
 
+        backgroundImage = (ImageView) findViewById(R.id.backgroundImage);
+
         parentLayout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        drawAttrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeDrawAttribs();
+            }
+        });
 
         pencilButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -199,5 +231,28 @@ public class EditImageActivity extends Activity {
         params.height = params.height == 0 ? ViewGroup.LayoutParams.MATCH_PARENT : 0;
 
         rightLayout.setLayoutParams(params);
+    }
+
+    private void changeDrawAttribs() {
+        DrawAttrDialog drawAttribsDialog = DrawAttrDialog.newInstance();
+        drawAttribsDialog.setPaint(myDrawView.getCurrentPaintParams());
+        drawAttribsDialog.setOnCustomViewDialogListener(new DrawAttrDialog.OnCustomViewDialogListener() {
+            @Override
+            public void onRefreshPaint(Paint newPaint) {
+                myDrawView.setDrawColor(newPaint.getColor())
+                        .setPaintStyle(newPaint.getStyle())
+                        .setDither(newPaint.isDither())
+                        .setDrawWidth((int) newPaint.getStrokeWidth())
+                        .setDrawAlpha(newPaint.getAlpha())
+                        .setAntiAlias(newPaint.isAntiAlias())
+                        .setLineCap(newPaint.getStrokeCap())
+                        .setFontFamily(newPaint.getTypeface())
+                        .setFontSize(newPaint.getTextSize());
+//                If you prefer, you can easily refresh new attributes using this method
+//                mDrawView.refreshAttributes(newPaint);
+            }
+        });
+        drawAttribsDialog.show(getSupportFragmentManager(), "drawAttribs");
+
     }
 }
